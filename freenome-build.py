94c702a892420533c12f60143b0cee3f75c652e1
drakeeee <adam.drake137@gmail.com>
2018-04-20 14:58:39 -0700

    add environment build and upload functionality

--- freenome-build.py
+++ freenome-build.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 import subprocess
 import conda
 import conda_build.api
@@ -25,6 +26,32 @@ def setup_development_environment(path='./', environment_name=None):
     )
     print(output_file_paths)
     assert False
+
+
+
+def environment_build_and_upload(path='./', upload=True):
+    # Set the environment variable VERSION so that
+    # the jinja2 templating works for the conda-build
+    with open('lims_api/__init__.py', 'r') as fp:
+        version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                            fp.read(), re.MULTILINE).group(1)
+    local_env = os.environ
+    local_env['VERSION'] = version
+
+    # build the package
+    yaml_fpath = os.path.normpath(os.path.abspath(os.path.join(path, "conda-build/meta.yaml")))
+
+    output_file_paths = conda_build.api.build(
+        [yaml_fpath,],
+        skip_existing=False
+    )
+
+    if upload:
+        upload_cmd = ['anaconda', '-t', local_env['ANACONDA_TOKEN'],
+                      'upload', '-u', 'freenome', output_file_paths[0]]
+
+        output = subprocess.check_output(upload_cmd)
+
 
 def main():
     if sys.argv[1] == 'develop':
