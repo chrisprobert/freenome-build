a4e0a5d3c66b06889b0700ae2e9f21e25d12a138
drakeeee <adam.drake137@gmail.com>
2018-05-02 15:18:28 -0700

    module for github functions

--- freenome_build/github_utils.py
+++
@@ -1,7 +0,0 @@
-def repo_name()
-    repo_name = os.path.basename(
-        subprocess.run(
-            'git rev-parse --show-toplevel',
-            shell=True,
-            stdout=subprocess.PIPE
-        ).stdout.strip().decode('utf8')).lower().replace('-', '_')
