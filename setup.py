336b4cd4de90377dbf02e4433f8923b9191eed86
Nathan Boley <npboley@gmail.com>
2018-05-21 09:19:24 -0700

    Moved freenome-build entry point into ./bin

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import os
+from setuptools import setup, find_packages
+
 try:
     from pip.download import PipSession
     from pip.req import parse_requirements
@@ -5,8 +8,6 @@ except ImportError:
     from pip._internal.download import PipSession
     from pip._internal.req import parse_requirements
 
-import os
-from setuptools import setup, find_packages
 
 VERSION_FILEPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), './VERSION'))
 
@@ -30,11 +31,7 @@ def main():
           install_requires=reqs,
           include_package_data=True,
           packages=find_packages(),
-          entry_points={
-              'console_scripts': [
-                  'freenome-build = freenome_build.freenome_build:main'
-              ]
-          })
+          scripts=['bin/freenome-build'])
 
 
 if __name__ == '__main__':
