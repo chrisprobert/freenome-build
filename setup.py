3c65b02808621e1ec4ea037d675d7d1fbad76430
Nathan Boley <npboley@gmail.com>
2018-06-02 09:00:00 -0700

    Added a symlink to VERSION. (#18)
    * Added a symlink for VERSION.
    * minor setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup, find_packages
+import setuptools
 
 try:
     from pip.download import PipSession
@@ -25,22 +25,25 @@ def main():
     install_reqs = parse_requirements("update_requirements.txt", session=PipSession())
     reqs = [str(ir.req) for ir in install_reqs if ir.req]
 
-    setup(name='freenome-build',
-          version=version,
-          description='Freenome build',
-          url='https://github.com/freenome/freenome-build',
-          author='Nathan Boley',
-          author_email="nathan.boley@freenome.com",
-          install_requires=reqs + ["pytest"],
-          package_data={
-              '': ['./VERSION'],
-              'freenome_build': [
-                  './database_template/*',
-                  './database_template/*/*',
-              ]
-          },
-          packages=find_packages(),
-          scripts=['bin/freenome-build'])
+    setuptools.setup(
+        name='freenome-build',
+        version=version,
+        description='Freenome build',
+        url='https://github.com/freenome/freenome-build',
+        author='Nathan Boley',
+        author_email="nathan.boley@freenome.com",
+
+        install_requires=reqs + ["pytest"],
+        package_data={
+            '': ['./VERSION'],
+            'freenome_build': [
+                './database_template/*',
+                './database_template/*/*',
+            ]
+        },
+        packages=setuptools.find_packages(),
+        scripts=['bin/freenome-build']
+    )
 
 
 if __name__ == '__main__':
