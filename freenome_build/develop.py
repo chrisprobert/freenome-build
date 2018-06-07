120c56b3b8dcb7aa6c91863c3f4a20c5563f2db4
Nathan Boley <npboley@gmail.com>
2018-06-07 11:49:12 -0700

    Fixed another bug.

--- freenome_build/develop.py
+++ freenome_build/develop.py
@@ -44,9 +44,7 @@ def setup_development_environment(path):
     local_channel = "file://" + os.path.split(os.path.split(output_file_path)[0])[0]
     run_and_log(f"conda install {package_name}=={version} --only-deps --yes -c {local_channel}")
     # python setup.py develop $PATH
-    run_and_log("python {} develop --single-version-externally-managed --record=record.txt".format(
-        norm_abs_join_path(path, "./setup.py"))
-    )
+    run_and_log("conda develop {path}".format(path=path))
 
 
 def add_develop_subparser(subparsers):
