6815cb6ef6ea6d82f76b119a167bce539a17bacf
Nathan Boley <npboley@gmail.com>
2018-06-08 14:40:42 -0700

    More updates to install script.

--- scripts/install_latest_stable_freenome_build.sh
+++ scripts/install_latest_stable_freenome_build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 ANACONDA_TOKEN=$1
 MINICONDA_URL=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 MINICONDA_INSTALL_PATH=$HOME/miniconda
 
+mkdir -p $MINICONDA_INSTALL_PATH;
 pushd $MINICONDA_INSTALL_PATH;
 
 # check for conda install, and install if neessary
@@ -11,10 +14,10 @@ CONDA_INSTALLED=1
 command -v conda >/dev/null 2>&1 || CONDA_INSTALLED=0
 if [[ $ANACONDA_INSTALLED -eq 0 ]]; then
     # install conda
-    mkdir -p $MINICONDA_INSTALL_PATH;
-    wget $MINICONDA_URL;
+    wget -q $MINICONDA_URL;
     bash $( basename $MINICONDA_URL ) -b -u -p $MINICONDA_INSTALL_PATH;
-    export PATH="$MINICONDA_INSTALL_PATH/bin:$PATH";
+    # activate the base conda environment
+    source activate;
 fi
 
 # setup the condarc with the correct set of channels
