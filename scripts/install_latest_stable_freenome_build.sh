a5365a444956182e47d61b79658cc09806803979
Nathan Boley <npboley@gmail.com>
2018-06-08 12:21:14 -0700

    fixed script bug.

--- scripts/install_latest_stable_freenome_build.sh
+++ scripts/install_latest_stable_freenome_build.sh
@@ -4,13 +4,14 @@ ANACONDA_TOKEN=$1
 MINICONDA_URL=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 MINICONDA_INSTALL_PATH=$HOME/miniconda
 
+pushd $MINICONDA_INSTALL_PATH;
+
 # check for conda install, and install if neessary
 CONDA_INSTALLED=1
 command -v conda >/dev/null 2>&1 || CONDA_INSTALLED=0
 if [[ $ANACONDA_INSTALLED -eq 0 ]]; then
     # install conda
     mkdir -p $MINICONDA_INSTALL_PATH;
-    cd $MINICONDA_INSTALL_PATH;
     wget $MINICONDA_URL;
     bash $( basename $MINICONDA_URL ) -b -u -p $MINICONDA_INSTALL_PATH;
     export PATH="$MINICONDA_INSTALL_PATH/bin:$PATH";
@@ -27,3 +28,5 @@ conda config --add channels https://conda.anaconda.org/t/$ANACONDA_TOKEN/freenom
 
 # install freenome-build
 conda install freenome-build --yes
+
+popd;
